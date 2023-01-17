c72de0808a996341530008b9c2791f414576ffbf
Krzysiek Błaszczyk <krzysztofblaszczyk@Krzysztofs-MacBook-Pro.local>
2023-01-17 21:39:08 +0100

    TASK 32.1

--- src/main/java/com/crud/tasks/controller/StaticWebPageController.java
+++ src/main/java/com/crud/tasks/controller/StaticWebPageController.java
@@ -1,11 +1,12 @@
 package com.crud.tasks.controller;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
 
-@Component
+@Controller
 public class StaticWebPageController {
     @RequestMapping("/")
     public String index(Map<String, Object> model) {
