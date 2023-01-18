959d6bc4edb35739ac67e03691af1cecca4384fc
Krzysiek Błaszczyk <krzysztofblaszczyk@Krzysztofs-MacBook-Pro.local>
2023-01-18 17:45:24 +0100

    TASK 32.3

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class MailCreatorService {
@@ -19,15 +22,21 @@ public class MailCreatorService {
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "https://krzysiekblaszczyk.github.io");
         context.setVariable("button", "Visit website");
-        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("admin_name", adminConfig);
         context.setVariable("company_name", "Kodilla");
         context.setVariable("goodbye_message", "Thank you for using our application");
         context.setVariable("show_button", false);
         context.setVariable("if_friend", true);
+        context.setVariable("application_functionality", functionality);
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
