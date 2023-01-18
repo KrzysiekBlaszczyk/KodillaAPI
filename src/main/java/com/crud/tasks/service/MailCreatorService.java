8f9703c83a8ca3c28f97e5bd80e05de82b4f924f
Krzysiek Błaszczyk <krzysztofblaszczyk@Krzysztofs-MacBook-Pro.local>
2023-01-18 18:10:02 +0100

    TASK 32.3

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,6 +1,8 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -21,6 +23,9 @@ public class MailCreatorService {
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
 
+    @Autowired
+    private TaskRepository taskRepository;
+
     public String buildTrelloCardEmail(String message) {
         List<String> functionality = new ArrayList<>();
         functionality.add("You can manage your tasks");
@@ -36,6 +41,23 @@ public class MailCreatorService {
         context.setVariable("goodbye_message", "Thank you for using our application");
         context.setVariable("show_button", false);
         context.setVariable("if_friend", true);
+        context.setVariable("application_functionality", functionality);
+        return templateEngine.process("mail/created-trello-card-mail", context);
+    }
+
+
+    public String buildTrelloScheduledEmail(String message) {
+        List<Task> functionality = taskRepository.findAll();
+
+        Context context = new Context();
+        context.setVariable("message", message);
+        context.setVariable("tasks_url", "https://krzysiekblaszczyk.github.io");
+        context.setVariable("button", "Visit website");
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("company_name", "Kodilla");
+        context.setVariable("goodbye_message", "Thank you for using our application");
+        context.setVariable("show_button", false);
+        context.setVariable("if_friend", false);
         context.setVariable("application_functionality", functionality);
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
