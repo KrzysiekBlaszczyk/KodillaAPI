df3975375fd23323c937bf800d69805e2be8e162
Krzysiek Błaszczyk <krzysztofblaszczyk@Krzysztofs-MacBook-Pro.local>
2023-01-18 15:10:52 +0100

    TASK 32.2

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.config.AdminConfig;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -12,14 +13,17 @@ import org.thymeleaf.context.Context;
 public class MailCreatorService {
 
     @Autowired
+    private AdminConfig adminConfig;
+    @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
         Context context = new Context();
         context.setVariable("message", message);
-        context.setVariable("tasks_url", "http://localhost:8888/crud");
+        context.setVariable("tasks_url", "https://krzysiekblaszczyk.github.io");
         context.setVariable("button", "Visit website");
+        context.setVariable("admin_name", adminConfig.getAdminName());
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
